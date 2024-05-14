95f72296c6a99c89399c35f97eae76f0822696dd
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2024-05-15 00:48:24 +0900

    faet: create post response

--- api/src/functions/message.ts
+++ api/src/functions/message.ts
@@ -3,9 +3,26 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function message(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
+    switch (request.method) {
+        case "GET":
+            return await getMessage(request, context);            
+        case "POST":
+            return await getMessage(request, context);            
+        default:
+            return { body: `Hello!!!` };
+    }
+};
+
+async function getMessage(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     const name = request.query.get('name') || await request.text() || 'world';
 
     return { body: `Hello, ${name}!` };
+};
+
+async function postMessage(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Updated by ${name}!` };
 };
 
 app.http('message', {
